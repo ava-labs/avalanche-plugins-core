28acddff0fceac9fb064220fe47850bccf34dc0f
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-03-29 13:51:36 -0400

    fixing spaces subnet + vm

--- plugin/subnet/spaces.go
+++ plugin/subnet/spaces.go
@@ -1,1 +1,86 @@
 package subnet
+
+import (
+	"os/exec"
+
+	"github.com/ava-labs/apm/definition"
+	"github.com/ava-labs/avalanchego/chains"
+	"github.com/ava-labs/avalanchego/snow/consensus/avalanche"
+	"github.com/ava-labs/avalanchego/snow/consensus/snowball"
+	"github.com/ava-labs/avalanchego/snow/networking/sender"
+	"github.com/ava-labs/avalanchego/version"
+
+	"github.com/ava-labs/avalanche-plugins-core/plugin/vm"
+)
+
+var _ definition.Subnet = &Spaces{}
+
+type Spaces struct {
+}
+
+func (s Spaces) ID() string {
+	return "Ai42MkKqk8yjXFCpoHXw7rdTWSHiKEMqh5h8gbxwjgkCUfkrk"
+}
+
+func (s Spaces) Alias() string {
+	return "spaces"
+}
+
+func (s Spaces) Homepage() string {
+	return "https://tryspaces.xyz"
+}
+
+func (s Spaces) Description() string {
+	return "Spaces enables authenticated, hierarchical storage of arbitrary " +
+		"keys/values using any EIP-712 compatible wallet."
+}
+
+func (s Spaces) Maintainers() []string {
+	return []string{"patrickogrady@avalabs.org"}
+}
+
+func (s Spaces) BeforeInstall() error {
+	return nil
+}
+
+func (s Spaces) Install() error {
+	cmd := exec.Command("./scripts/build.sh")
+	_, err := cmd.Output()
+	return err
+}
+
+func (s Spaces) AfterInstall() error {
+	return nil
+}
+
+func (s Spaces) VMs() map[definition.VM]version.Version {
+	return map[definition.VM]version.Version{
+		vm.Spaces{}: vm.Spaces{}.Version(),
+	}
+}
+
+func (s Spaces) SubnetConfig() *chains.SubnetConfig {
+	return &chains.SubnetConfig{
+		GossipConfig: sender.GossipConfig{
+			AcceptedFrontierSize:      10,
+			OnAcceptSize:              10,
+			AppGossipNonValidatorSize: 10,
+			AppGossipValidatorSize:    10,
+		},
+		ValidatorOnly: false,
+		ConsensusParameters: avalanche.Parameters{
+			Parameters: snowball.Parameters{
+				K:                     10,
+				Alpha:                 10,
+				BetaVirtuous:          10,
+				BetaRogue:             10,
+				ConcurrentRepolls:     10,
+				OptimalProcessing:     10,
+				MaxOutstandingItems:   10,
+				MaxItemProcessingTime: 10,
+			},
+			Parents:   10,
+			BatchSize: 10,
+		},
+	}
+}
--- plugin/vm/spaces.go
+++ plugin/vm/spaces.go
@@ -1,7 +1,64 @@
 package vm
 
-import "github.com/ava-labs/avalanchego-internal/database"
+import (
+	"github.com/ava-labs/apm/definition"
+	"github.com/ava-labs/avalanchego/version"
+)
+
+var _ definition.VM = &Spaces{}
 
 type Spaces struct {
-	db database.Database
+}
+
+func (s Spaces) ID() string {
+	return "tGas3T58KzdjLHhBDMnH2TvrddhqTji5iZAMZ3RXs2NLpSnhH"
+}
+
+func (s Spaces) Alias() string {
+	return "spacesvm"
+}
+
+func (s Spaces) Homepage() string {
+	return "https://tryspaces.xyz"
+}
+
+func (s Spaces) Description() string {
+	return "Spaces enables authenticated, hierarchical storage of arbitrary " +
+		"keys/values using any EIP-712 compatible wallet."
+}
+
+func (s Spaces) Maintainers() []string {
+	return []string{"patrickogrady@avalabs.org"}
+}
+
+func (s Spaces) BeforeInstall() error {
+	return nil
+}
+
+func (s Spaces) Install() error {
+	return nil
+}
+
+func (s Spaces) AfterInstall() error {
+	return nil
+}
+
+func (s Spaces) Version() version.Version {
+	return version.NewDefaultVersion(0, 0, 3)
+}
+
+func (s Spaces) Repository() string {
+	return "https://github.com/ava-labs/spacesvm.git"
+}
+
+func (s Spaces) SHA256() string {
+	panic("implement me")
+}
+
+func (s Spaces) Commit() string {
+	return "10f885ffc2770951bb0fd4d2b4b658c3b9cc5caa"
+}
+
+func (s Spaces) Branch() string {
+	return "master"
 }
