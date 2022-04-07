514e415fae8886d4a0d7d767f5f9a392c12e2a6c
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-04-07 15:29:52 -0400

    init

---
+++ main/init.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"encoding/gob"
+
+	"github.com/ava-labs/avalanche-plugins-core/plugin/subnet"
+	"github.com/ava-labs/avalanche-plugins-core/plugin/vm"
+)
+
+func init() {
+	gob.Register(&subnet.Spaces{})
+	gob.Register(&vm.Spaces{})
+}
